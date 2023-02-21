2f913fe11954026b80eccb36214ede1eb5c86793
Mahmoud Mohamed Ali ALi <99610976+iMahmouddAli@users.noreply.github.com>
2023-02-21 10:40:25 +0200

    add courses and department procedures

--- Tables/StaticData/dbo.Courses.Data.sql
+++ Tables/StaticData/dbo.Courses.Data.sql
@@ -5,3 +5,5 @@ INSERT dbo.Courses(Crs_id, Name, Duration, Description) VALUES (400, 'MongoDB', 
 INSERT dbo.Courses(Crs_id, Name, Duration, Description) VALUES (500, 'NodeJS', 30, 'NodeJS');
 INSERT dbo.Courses(Crs_id, Name, Duration, Description) VALUES (600, 'C', 30, 'C Programming');
 INSERT dbo.Courses(Crs_id, Name, Duration, Description) VALUES (700, 'DS', 10, 'Data Structure');
+
+INSERT dbo.Courses(Crs_id, Name, Duration, Description) VALUES (800, 'DM', 60, 'Data Maining');
